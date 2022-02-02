18714a2c48bf7be6a9088a876aca7be8fae1a024
dongpakka <kimvjgd@naver.com>
2022-02-02 21:38:51 +0900

    second_commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_getx/app_controller.dart';
 import 'package:flutter_getx/first_controller.dart';
 import 'package:flutter_getx/home.dart';
+import 'package:flutter_getx/home_controller_binding.dart';
 import 'package:flutter_getx/second_controller.dart';
 import 'package:get/get.dart';
 
 void main() {
+  AppController().dependencies();
   runApp(MyApp());
 }
 
@@ -14,8 +17,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      initialBinding: SecondController(),
+      // initialBinding: SecondController(),
       title: " Binding GetX ",
+      // getPages: [
+      //   GetPage(name: '/home', page: ()=>Home(),binding: HomeControllerBinding()),
+      // ],
+      getPages: [
+        GetPage(name: '/home', page: ()=>Home(), binding: BindingsBuilder(() => {
+          Get.lazyPut<HomeControllerBinding>(() => HomeControllerBinding()),
+        }))
+      ],
       home: Scaffold(
         appBar: AppBar(title: Text(' Binding GetX '),),
         body: Center(
@@ -32,7 +43,9 @@ class MyApp extends StatelessWidget {
               }, child: Text(" Increment Button ")),
               SizedBox(height: 10.0,),
               ElevatedButton(onPressed: (){
-                Get.to(Home());
+                // Get.to(Home());
+                // Get.toNamed("/home");
+                Get.to(Home(), binding: HomeControllerBinding());
               }, child: Text("Home"))
 
             ],
